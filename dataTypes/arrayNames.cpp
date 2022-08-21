e68e7ff8834ee76838029ea1ead26c1ce45be23d
andrejarboe <andrejarboe@gmail.com>
2022-08-21 14:14:11 -0400

    array names

---
+++ dataTypes/arrayNames.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    string names[3];
+
+    cout << "Name: ";
+    getline(cin, names[0]);
+
+    cout << "Name: ";
+    getline(cin, names[1]);
+
+    cout << "Name: ";
+    getline(cin, names[2]);
+
+
+    cout << names[0];
+
+    return 0;
+}
